54265ed856ba16b9ceb3f05f01ace923432b335c
Ravikeerthy <ravikeerthy1807@gmail.com>
2024-09-25 16:49:43 +0400

    headers changed

--- middleWare/authMiddleware.js
+++ middleWare/authMiddleware.js
@@ -4,7 +4,7 @@ import User from "../models/user.schema.js";
 dotenv.config();
 
 const authMiddleWare = async (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
+  const token = req.cookies.token;
 
   if (!token) {
     return res.status(401).json({ message: "Authorization header is missing" });
