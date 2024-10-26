85809555dfd6330c002e1d83a1e5c4e33022cfa9
Ravikeerthy <ravikeerthy1807@gmail.com>
2024-10-26 14:43:47 +0400

    details added

--- generatePDF/generateExcel.js
+++ generatePDF/generateExcel.js
@@ -11,15 +11,62 @@ export const createExcelReport = async (req, res) => {
       { header: "Category", key: "category", width: 15 },
       { header: "Amount", key: "amount", width: 10 },      
       { header: "Source", key: "source", width: 10 },
+      { header: "Description", key: "description", width: 10 },
       { header: "Date", key: "date", width: 10 },
       { header: "Frequency", key: "frequency", width: 10 },
-      { header: "Description", key: "description", width: 10 },
     ];
 
-    workSheet.addRow({ category: "Income", amount: income.incomeAmount || 0 });
-    workSheet.addRow({ category: "Expense", amount: expense.expenseAmount ||0});
-    workSheet.addRow({ category: "Saving", amount: saving.savingAmount || 0});
-    workSheet.addRow({ category: "Budget", amount: budget.budgetAmount || 0});
+    income.forEach((item) =>
+      workSheet.addRow({
+        category: "Income",
+        amount: item.incomeAmount || 0,
+        source: item.source || "",
+        date: item.date || "",
+        frequency: item.frequency || "",
+        description: item.description || "",
+      })
+    );
+
+    expense.forEach((item) =>
+      workSheet.addRow({
+        category: "Expense",
+        amount: item.expenseAmount || 0,
+        source: item.source || "",
+        date: item.date || "",
+        frequency: item.frequency || "",
+        description: item.description || "",
+      })
+    );
+
+    saving.forEach((item) => {
+      workSheet.addRow({
+        category: "Saving",
+        amount: item.savingAmount || 0,
+        source: item.source || "",
+        date: item.date || "",
+        frequency: item.frequency || "",
+        description: item.description || "",
+      });
+    });
+
+    
+    budget.forEach((item) => {
+      workSheet.addRow({
+        category: "Budget",
+        amount: item.budgetAmount || 0,
+        source: item.source || "",
+        date: item.date || "",
+        frequency: item.frequency || "",
+        description: item.description || "",
+      });
+    });
+
+
+
+    // workSheet.addRow({ category: "Income", amount: income.incomeAmount || 0 });
+    // workSheet.addRow({ category: "Expense", amount: expense.expenseAmount ||0});
+    // workSheet.addRow({ category: "Saving", amount: saving.savingAmount || 0});
+    // workSheet.addRow({ category: "Budget", amount: budget.budgetAmount || 0});
 
     const buffer = await workbook.xlsx.writeBuffer();
 
