f5ca322d6106d562f977767b5f46801b572159e0
Ravikeerthy <ravikeerthy1807@gmail.com>
2024-10-26 23:20:57 +0400

    token

--- utils/tokenGenerate.js
+++ utils/tokenGenerate.js
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const generateToken = (user) =>{
-  return jwt.sign({ _id: user._id }, process.env.SECRET_KEY, { expiresIn: "1min" });
+  return jwt.sign({ _id: user._id }, process.env.SECRET_KEY, { expiresIn: "1h" });
 }
 export default generateToken;
